8d1643a85773e1f6b99eef48cc12c8017639119f
yulia-gutorova <j.gutorova@gmail.com>
2022-11-19 11:12:01 +0100

    server, base url, get all items

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
 const fs = require('fs');
+const port = 8080;
 
 app.use(bodyParser.json());
 
@@ -9,3 +10,38 @@ app.use(bodyParser.json());
 let rowData = fs.readFileSync('data.json');
 let data = JSON.parse(rowData);
 console.log(data);
+
+//-----------------------------------------------------------------------------
+
+/*********************************************************** 
+/ Base URL
+************************************************************/
+
+app.get('/', function (req, res) 
+{
+  res.send('Hello World');
+})
+
+/*********************************************************** 
+/ Return all items
+************************************************************/
+
+app.get('/items', function (req, res) 
+{
+    res.status(200).json(data);
+})
+
+
+//-----------------------------------------------------------------------------
+//Server listener
+app.listen(port, err => 
+{
+    if (err) 
+    {
+        return console.log('Error', err);
+    }
+    else
+    {
+        console.log('Server listening on port 8080...');
+    }
+});
