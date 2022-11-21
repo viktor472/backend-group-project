40bb1a232239470e5f9214134a86f21015f3f75d
yulia-gutorova <j.gutorova@gmail.com>
2022-11-21 14:58:48 +0100

    return _id back + find a specific item by custom id

--- routes/items.js
+++ routes/items.js
@@ -33,10 +33,9 @@ router.post('/', async function (req, res)
     //get data from request
     const item = new Item
     ({       
-            _id: req.body.id,                  // get id from POST req
+            id: req.body.id,                  // get id from POST req
             title: req.body.title,            // get title from POST req       
     })
-
 
     try{
         console.log('Inside post item function');
@@ -50,14 +49,16 @@ router.post('/', async function (req, res)
 })
 
 /*********************************************************** 
-/ Get a specific item
+/ Get a specific item by custom id
 ************************************************************/
 router.get('/:id', async function (req, res)
 {
     //res.status(200).json(data);
     try{
         console.log('Inside get a specific item function');
-        const item = await Item.findById(parseInt(req.params.id));
+        //const item = await Item.findById(parseInt(req.params.id));
+        //{"yourFieldName":yourValue}
+        const item = await Item.find({"id":parseInt(req.params.id)})
         if (item==null)
         {
             return res.status(404).json({message: 'The item does not exist'})
@@ -70,6 +71,42 @@ router.get('/:id', async function (req, res)
     catch(err){
         res.status(500).json({message: err.message});
     } 
+})
+
+/*********************************************************** 
+/ Update existing item
+************************************************************/
+
+router.put('/:id', async function (req, res)
+{
+    try{
+        console.log('Inside uddate existing item function');
+        let item = await Item.findById(parseInt(req.params.id));
+        if (item==null)
+        {
+            return res.status(404).json({message: 'The item does not exist'})
+        }
+        else 
+        {
+            //-----------------------------------------
+                //get data from request
+                item.title = req.body.title;
+
+                try{
+                    //console.log('Inside post item function');
+                    //save new item in the datadase
+                    const uppdatedItem = await item.save();
+                    res.status(201).json(uppdatedItem);
+                }
+                catch(err){
+                    res.status(400).json({message: err.message});
+                }
+            //-----------------------------------------
+        }       
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }  
 })
 
 
