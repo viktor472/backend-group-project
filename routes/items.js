6b283ca1274848c1ccfc0f006e4e76b20178b166
yulia-gutorova <j.gutorova@gmail.com>
2022-11-27 20:30:29 +0100

    functiondelete item from database works

--- routes/items.js
+++ routes/items.js
@@ -31,16 +31,21 @@ router.get('/', async function (req, res)
 router.post('/', async function (req, res)
 {
     //get data from request
-    const item = new Item
-    ({       
-            id: req.body.id,                  // get id from POST req
-            title: req.body.title,            // get title from POST req       
-    })
-    console.log(item);
+    // const item = new Item
+    // ({       
+    //         id: req.body.id,                  // get id from POST req
+    //         title: req.body.title,            // get title from POST req       
+    // })
+    // console.log(item);
     try{
         console.log('Inside post item function');
         //save new item in the datadase
-        const newItem = await item.save();
+       // const newItem = await item.save();
+
+       const newItem = await Item.create({
+          id: req.body.id,                  // get id from POST req
+          title: req.body.title,
+       })
         res.status(201).json(newItem);
     }
     catch(err){
@@ -56,14 +61,14 @@ router.get('/:id', async function (req, res)
     //res.status(200).json(data);
     try{
         console.log('Inside get a specific item function');
-        const items = await Item.find({"id":parseInt(req.params.id)});
-        if (items.length == 0)
+        const item = await Item.findOne({"id":parseInt(req.params.id)});
+        if (!item)
         {
             return res.status(404).json({message: 'The item does not exist'})
         }
         else 
         {
-            res.status(200).json(items);
+            res.status(200).json(item );
         }       
     }
     catch(err){
@@ -77,18 +82,35 @@ router.get('/:id', async function (req, res)
 
 router.put('/:id', async function (req, res)
 {
-    let items = await Item.find({"id":parseInt(req.params.id)});
-    console.log(items);
-    try{
-        console.log('Inside post item function');
-        //save new item in the datadase
-        await Item.updateOne({"id":parseInt(req.params.id)}, {title:req.body.title});
-        items = await Item.find({"id":parseInt(req.params.id)});
-        res.status(201).json(items);
+    let item = await Item.findOne({"id":parseInt(req.params.id)});
+    console.log(item);
+
+    if (!item) {
+        res.status(400).json({message: 'The item does not exist'});
     }
-    catch(err){
-        res.status(400).json({message: err.message});
-    } 
+    else {
+        try{
+            console.log('Inside PUT item function'); 
+            let item = await Item.findOne({"id":parseInt(req.params.id)});
+            console.log(item);
+            if (!item) 
+                {
+                res.status(400).json({message: 'The item does not exist'});
+                 }      
+            //save new item in the datadase
+            else {await Item.updateOne(
+                {"id":parseInt(req.params.id)}, 
+                {title:req.body.title});
+    
+            item = await Item.findOne({"id":parseInt(req.params.id)});
+            res.status(201).json(item);
+            }
+        }
+        catch(err){
+            res.status(400).json({message: err.message});
+        } 
+    }
+
 })
 
 /*********************************************************** 
@@ -98,18 +120,21 @@ router.put('/:id', async function (req, res)
 router.delete('/:id', async function (req, res)
 {   
         console.log('Inside delete a specific item function');
-        //const item = await Item.findById(parseInt(req.params.id));
-        //{"yourFieldName":yourValue}
-        let items = await Item.find({"id":parseInt(req.params.id)});
-        console.log(items);
-        const item = items[0];
-        console.log(item);
+        let item = await Item.findOne({"id":parseInt(req.params.id)});
 
-        console.log('Inside delete try ');
-        //save new item in the datadase
+        if (!item) {
+            res.status(400).json({message: 'The item does not exist'});
+        }        
+        else {
+            try {
+                console.log(item);
+                await item.remove();
+                res.status(200).json({message: "Item deleted"})  
+            } catch (error) {
+                res.status(500).json({message: err.message});
+            }
+        }
     
-      
 })
-
 
 module.exports = router;
