8b23de4b2340c324595099da675f3e9add694f35
viktor472 <viktor.alex.ceder@gmail.com>
2022-11-29 11:47:45 +0100

    test

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,45 +1,42 @@
-let posts = []
+let posts = [];
 
 //create blog post (Viktor)
 exports.postUser = (req, res) => {
-  let title = req.body.title
-  let article = req.body.article
+  let title = req.body.title;
+  let article = req.body.article;
   posts.push({
     title: title,
     article: article,
-    id: (users.length + 1).toString()
-  })
+    id: (users.length + 1).toString(),
+  });
   res.status(200).json({
-    msg: `Post upploaded!`
-  })
-}
+    msg: `Post upploaded!`,
+  });
+};
 
-//Update blog post (Sina)
 exports.updateUser = (req, res) => {
-  // Read from head and body
-  let id = req.params.id
-  let title = req.body.title
-  let article = req.body.article
+  let id = req.params.id;
+  let title = req.body.title;
+  let article = req.body.article;
 
-  // Check title and article fields not empty
   if (title && article) {
-    let index = posts.findIndex((upd) => upd.id == id)
+    let index = posts.findIndex((upd) => upd.id == id);
 
     posts[index] = {
       ...posts[index],
       title,
-      article
-    }
+      article,
+    };
 
     res.status(200).json({
-      msg: 'Blog updated',
-      status: 'success',
-      data: posts
-    })
+      msg: "Blog updated",
+      status: "success",
+      data: posts,
+    });
   } else {
     res.status(400).json({
-      msg: 'Validation error',
-      status: 'Failed'
-    })
+      msg: "Validation error",
+      status: "Failed",
+    });
   }
-}
+};
