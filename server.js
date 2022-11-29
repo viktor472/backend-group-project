c9e4e1fe181b25ded5cd4ed379d089ba21201899
AD1121 <sinagh30@gmail.com>
2022-11-29 11:57:13 +0100

    Changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
 //midle weare
-app.use(express.json());
-app.use("/api/blog", require("./routes/blogControler"));
+app.use(express.json())
+app.use('/api/blog', require('./routes/blogController'))
 
 //midle weare end
 
 app.listen(PORT, (err) => {
-  console.log(`Listening on port: ${PORT}`);
-});
+  console.log(`Listening on port: ${PORT}`)
+})
