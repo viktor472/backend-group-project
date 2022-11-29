92817f94c4a48b640b2fa2f349e7a8b470143ebf
AD1121 <sinagh30@gmail.com>
2022-11-29 11:55:37 +0100

    Comments added

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,50 +1,55 @@
-let posts = [];
+let posts = []
+
 //get blog posts (Viktor)
 exports.getpost = (req, res) => {
   res.status(200).json({
-    msg: "Post:",
+    msg: 'Post:',
     result: posts.length,
-    status: "seuccess",
-    data: posts,
-  });
-};
+    status: 'seuccess',
+    data: posts
+  })
+}
+
 //create blog post (Viktor)
-exports.postpost = (req, res) => {
-  let title = req.body.title;
-  let article = req.body.article;
+exports.postBlog = (req, res) => {
+  let title = req.body.title
+  let article = req.body.article
   posts.push({
     title: title,
     article: article,
-    id: (users.length + 1).toString(),
-  });
+    id: (users.length + 1).toString()
+  })
   res.status(200).json({
-    msg: `Post upploaded!`,
-  });
-};
+    msg: `Post upploaded!`
+  })
+}
 
-exports.updateUser = (req, res) => {
-  let id = req.params.id;
-  let title = req.body.title;
-  let article = req.body.article;
+//update blog post (Sina)
+exports.updateBlog = (req, res) => {
+  // Adding the header and body to the variables
+  let id = req.params.id
+  let title = req.body.title
+  let article = req.body.article
 
+  // Check title and article not empty
   if (title && article) {
-    let index = posts.findIndex((upd) => upd.id == id);
+    let index = posts.findIndex((upd) => upd.id == id)
 
     posts[index] = {
       ...posts[index],
       title,
-      article,
-    };
+      article
+    }
 
     res.status(200).json({
-      msg: "Blog updated",
-      status: "success",
-      data: posts,
-    });
+      msg: 'Blog updated',
+      status: 'success',
+      data: posts
+    })
   } else {
     res.status(400).json({
-      msg: "Validation error",
-      status: "Failed",
-    });
+      msg: 'Validation error',
+      status: 'Failed'
+    })
   }
-};
+}
