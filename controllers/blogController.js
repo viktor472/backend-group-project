a367816cc9c649b56a49865aa253f40d769aec52
AD1121 <sinagh30@gmail.com>
2022-11-29 11:32:41 +0100

    Update blog added

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,15 +1,42 @@
-let posts = [];
+let posts = []
 
 //create blog post (Viktor)
 exports.postUser = (req, res) => {
-  let title = req.body.title;
-  let article = req.body.article;
+  let title = req.body.title
+  let article = req.body.article
   posts.push({
     title: title,
     article: article,
-    id: (users.length + 1).toString(),
-  });
+    id: (users.length + 1).toString()
+  })
   res.status(200).json({
-    msg: `Post upploaded!`,
-  });
-};
+    msg: `Post upploaded!`
+  })
+}
+
+exports.updateUser = (req, res) => {
+  let id = req.params.id
+  let title = req.body.title
+  let article = req.body.article
+
+  if (title && article) {
+    let index = posts.findIndex((upd) => upd.id == id)
+
+    posts[index] = {
+      ...posts[index],
+      title,
+      article
+    }
+
+    res.status(200).json({
+      msg: 'Blog updated',
+      status: 'success',
+      data: posts
+    })
+  } else {
+    res.status(400).json({
+      msg: 'Validation error',
+      status: 'Failed'
+    })
+  }
+}
