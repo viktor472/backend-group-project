3f84b253e8dd968d48f2525aaf0c235325f7da82
viktor472 <viktor.alex.ceder@gmail.com>
2022-11-29 11:08:02 +0100

    initial-commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+//midle weare
+app.use(express.json());
+app.use("/api/blog", require("./routes/blogControler"));
+
+//midle weare end
+
+app.listen(PORT, (err) => {
+  console.log(`Listening on port: ${PORT}`);
+});
